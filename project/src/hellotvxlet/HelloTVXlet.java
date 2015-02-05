fd34cadbdb290e3700c4fd841f2266f7670de100
GieleCools <giele.cools@student.kdg.be>
2015-02-05 14:40:32 +0100

    Achtergrond in background plane gezet.

--- project/src/hellotvxlet/HelloTVXlet.java
+++ project/src/hellotvxlet/HelloTVXlet.java
@@ -1,6 +1,7 @@
 package hellotvxlet;
 
 import javax.tv.xlet.*;
+import org.davic.resources.ResourceProxy;
 import org.dvb.ui.*;
 import org.dvb.event.*;
 import java.awt.*;
@@ -8,52 +9,79 @@ import java.awt.event.*;
 import org.havi.ui.event.*;
 import org.havi.ui.*;
 import java.util.Timer;
-
+import org.davic.resources.*;
 
 //rechtermuisknop add abstract interface , er komt methode bij vanonvder waar alles in moetuserventRecieved
-public class HelloTVXlet implements Xlet, UserEventListener {
 
-    public HelloTVXlet() {
-        
-    }
+public class HelloTVXlet implements Xlet, UserEventListener, ResourceClient, HBackgroundImageListener
+{
     
-
     private XletContext actueleXletContext;
     private HScene scene;
     private boolean debug=true;
-    private MijnComponent schip = new MijnComponent("spaceship.png",0,0);
-    private MijnComponent sterren = new MijnComponent("sterren.png",0,0);
-     private MijnComponent sterren2 = new MijnComponent("sterren.png",0,-569);
+   
+    private HScreen screen;
+    private HBackgroundDevice bgDevice;
+    private HBackgroundConfigTemplate bgTemplate;
+    private HStillImageBackgroundConfiguration bgConfiguration;
+    private HBackgroundImage bgImage = new HBackgroundImage("backgroundimage.jpg");
     
-    private int schipx = 0;
-    private int schipy = 0;
-    
-    private int sterreny = 0;
+
     
     //---------------------
     
 
-    public void initXlet(XletContext xletContext) throws XletStateChangeException {
+    public void initXlet(XletContext xletContext) throws XletStateChangeException 
+    {
         
       if(debug) System.out.println("Xlet Initialiseren");
       this.actueleXletContext = xletContext;
 
-      scene= HSceneFactory.getInstance().getDefaultHScene();
-     //---------------------
-      scene.add(schip);
-      scene.add(sterren);
-      scene.add(sterren2);
-     
+      // HScreen object opvragen
+      screen = HScreen.getDefaultHScreen();
+      
+      // HBackgroundDevice opvragen
+      bgDevice = screen.getDefaultHBackgroundDevice();
+      
+      // HBackgroundDevice proberen te reserveren
+      if ( bgDevice.reserveDevice(this))
+      {
+          System.out.println("BackgroundImage device has been reserved.");
+      }
+      else
+      {
+          System.out.println("Background image device cannot be reserved.");
+      }
+      
+      // Template maken
+      bgTemplate = new HBackgroundConfigTemplate();
+      
+      // Configurateinstelling "STILL_IMAGE"
+      bgTemplate.setPreference(HBackgroundConfigTemplate.STILL_IMAGE, HBackgroundConfigTemplate.REQUIRED);
+      
+      // Configuratie aanvragen en activeren en OK
+      bgConfiguration = (HStillImageBackgroundConfiguration)bgDevice.getBestConfiguration(bgTemplate);
+      
+      try 
+      {
+          bgDevice.setBackgroundConfiguration(bgConfiguration);
+      }
+      catch (Exception s)
+      {
+          System.out.println(s.toString());
+      }
+      
       scene.validate();
       scene.setVisible(true);
-      
-      
       
     }
 
     public void startXlet() throws XletStateChangeException{
         
-    System.out.println("Xlet gestart");
+    if(debug) System.out.println("Xlet Starten");
+    
+    // Image laden
+    bgImage.load(this);
     
     EventManager manager = EventManager.getInstance();
     
@@ -61,7 +89,7 @@ public class HelloTVXlet implements Xlet, UserEventListener {
     
     repository.addKey(org.havi.ui.event.HRcEvent.VK_UP);
     repository.addKey(org.havi.ui.event.HRcEvent.VK_DOWN);
-     repository.addKey(org.havi.ui.event.HRcEvent.VK_LEFT);
+    repository.addKey(org.havi.ui.event.HRcEvent.VK_LEFT);
     repository.addKey(org.havi.ui.event.HRcEvent.VK_RIGHT);
     
     manager.addUserEventListener(this, repository);
@@ -69,49 +97,66 @@ public class HelloTVXlet implements Xlet, UserEventListener {
        
     }
 
-    public void pauseXlet() {
-     
-    }
-
-    public void destroyXlet(boolean unconditional) throws XletStateChangeException {
-     
-    }
-    
-    public void callback()
+    public void pauseXlet() 
     {
-        sterreny++;
-        sterren.verplaats(0, sterreny);
-        sterren2.verplaats(0,sterreny-569);
-        if(sterreny>569)
-        {
-            sterreny-=569;
-        }
-        
+        if(debug) System.out.println("Xlet Pauzeren");
     }
 
-    public void userEventReceived(org.dvb.event.UserEvent e) {
+    public void destroyXlet(boolean unconditional) throws XletStateChangeException 
+    {
+         if(debug) System.out.println("Xlet Destroy");
+          bgImage.flush();
+    }
+
+
+    public void userEventReceived(org.dvb.event.UserEvent e) 
+    {
        if(e.getType() == KeyEvent.KEY_PRESSED){
            System.out.println("Pushed Button");
            switch(e.getCode())
            {
                case HRcEvent.VK_LEFT:
-                   schipx -= 10;
-                   schip.verplaats(schipx,schipy);
+                   System.out.println("Left key pressed");
                    break;
                case HRcEvent.VK_RIGHT:
-                   schipx += 10;
-                   schip.verplaats(schipx,schipy);
+                   System.out.println("Right key pressed");
                    break;
                case HRcEvent.VK_UP:
-                   schipy -= 10;
-                   schip.verplaats(schipx,schipy);
+                   System.out.println("Up key pressed");
                    break;
                case HRcEvent.VK_DOWN:
-                   schipy += 10;
-                   schip.verplaats(schipx,schipy);
+                   System.out.println("Down key pressed");
                    break;
                    
            }
        }
+    }
+
+    public boolean requestRelease(ResourceProxy proxy, Object requestData) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void release(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void notifyRelease(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void imageLoaded(HBackgroundImageEvent e) {
+        try
+        {
+            bgConfiguration.displayImage(bgImage);
+        }    
+        catch(Exception s)
+        {
+            System.out.println(s.toString());
+        }
+    }
+
+    public void imageLoadFailed(HBackgroundImageEvent e) {
+        System.out.print("Image kan niet geladen worden.");
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
